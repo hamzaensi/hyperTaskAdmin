32311342cd6ff6a9bb3634e5645ba2f38bef8a85
hamza <hamzaensi@gmail.com>
2021-05-04 10:26:26 +0100

    Modify from php storm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hamza
+                    Laravel
                 </div>
 
                 <div class="links">
